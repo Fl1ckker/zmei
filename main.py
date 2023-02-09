b1037dc8e34544fe324c0b8a7333bc145345d7ac
Fl1ckker <1v@nv0vk>
2023-02-09 16:27:48 +0700

    1.4

--- main.py
+++ main.py
@@ -26,14 +26,30 @@ def drawzmei(screen, size, list):
                                      (j * 50 + 49, i * 50 + 49), (j * 50 + 1, i * 50 + 49)]
                                     , 0)
                 pygame.draw.polygon(screen, pygame.Color("black"),
-                                    [(g[1] * 50 + 20, i * 50 + 20), (j * 50 + 20, i * 50 + 30)
+                                    [(j * 50 + 20, i * 50 + 20), (j * 50 + 20, i * 50 + 30)
                                         , (j * 50 + 30, i * 50 + 30) , (j * 50 + 30, i * 50 + 20)]
                                     , 0)
             if list[i][j] == 3:
                 pygame.draw.circle(screen, pygame.Color("red"), (j * 50 + 25, i * 50 + 25), 24)
-                
+
+def tab(screen, size, records, score):
+    font = pygame.font.Font('Arcade.txt', 60)
+    fontt = pygame.font.Font('Arcade.txt', 48)
+    sct = font. render("score:" + str(score).zfill(4), True, (255, 255, 255))
+    screen.blit(sct, (size[0] + 20, 0))
+    for i in range(11):
+        rnt = fontt.render(str(i + 1) + "." + records[i][0] + ":" + records[i][1].zfill(4), True, (255, 255, 255))
+        screen.blit(rnt, (size[0] + 20, 55 + i * 50))
+
                 
 if __name__=='__main__':
+    file = open("records.txt", "r")
+    opens = file.readline().split("/--/")
+    records = []
+    for i in opens:
+        records.append(i.split("; "))
+    file.close()
+    score = 0
     pygame.init()
     w, h = 600, 600 #map(int, input().split())
     size = w, h
@@ -49,12 +65,13 @@ if __name__=='__main__':
         i1, j1 = random.randint(0, h // 50 - 1), random.randint(0, w // 50 - 1)
     list[i1][j1] = 3
     lis_z = [[0, 2], [0, 1], [0, 0]]
-    screen = pygame.display.set_mode(size)
+    screen = pygame.display.set_mode([size[0] + 350, size[1]])
     running = True
     x = 1
     y = 0
     drawline(screen, size)
     drawzmei(screen, size, list)
+    tab(screen, size, records, score)
     ti = time.time()
     while running:
         t = False
@@ -63,52 +80,61 @@ if __name__=='__main__':
                 running = False
             if event.type == pygame.KEYDOWN:
                 keys = pygame.key.get_pressed()
-                if keys[pygame.K_w]:
+                if keys[pygame.K_w] and y != 1:
                     x = 0
                     y = -1
                     t = True
-                elif keys[pygame.K_s]:
+                elif keys[pygame.K_s] and y != -1:
                     x = 0
                     y = 1
                     t = True
-                elif keys[pygame.K_a]:
+                elif keys[pygame.K_a] and x != 1:
                     x = -1
                     y = 0
                     t = True
-                elif keys[pygame.K_d]:
+                elif keys[pygame.K_d] and x != -1:
                     x = 1
                     y = 0
                     t = True
-        if t or time.time() - ti >= 1:
+        if t or time.time() - ti >= 0.75:
             ti = time.time()
             t = False
-            if g[0] + y < 0 or g[1] + x < 0 or g[0] + y == h // 50 or g[1] + x == w // 50:
-                print("Поражение")
-                exit()
-            if list[g[0] + y][g[1] + x] == 3:
+            if list[(g[0] + y) % (h // 50)][(g[1] + x) % (w // 50)] == 3:
+                score += 1
                 f = True
                 for i in list:
                     f *= all(i)
                 if f:
                     print("Победа!")
                     exit()
-                list[g[0]][g[1]] = 1
-                list[g[0] + y][g[1] + x] = 2
+                list[g[0] % (h // 50)][g[1] % (w // 50)] = 1
+                list[(g[0] + y) % (h // 50)][(g[1] + x) % (w // 50)] = 2
                 while list[i1][j1] == 1 or list[i1][j1] == 2:
                     i1, j1 = random.randint(0, h // 50 - 1), random.randint(0, w // 50 - 1)
                 list[i1][j1] = 3
-            elif list[g[0] + y][g[1] + x] == 1:
+            elif list[(g[0] + y) % (h // 50)][(g[1] + x) % (w // 50)] == 1:
                 print("Поражение")
+                records.append([str(random.choice(range(1000, 10**8))), str(score)])
+                records.sort(key= lambda x: int(x[1]), reverse=True)
+                save = []
+                for i in records:
+                    save.append("; ".join(i))
+                file = open("records.txt", "w")
+                file.write("/--/".join(save))
+                file.close()
                 exit()
             else:
-                list[g[0]][g[1]] = 1
-                list[g[0] + y][g[1] + x] = 2
+                list[g[0] % (h // 50)][g[1] % (w // 50)] = 1
+                list[(g[0] + y) % (h // 50)][(g[1] + x) % (w // 50)] = 2
                 k = lis_z.pop()
-                list[k[0]][k[1]] = 0
+                list[k[0] % (h // 50)][k[1] % (w // 50)] = 0
             g[0], g[1] = g[0] + y, g[1] + x
-            lis_z = [[g[0], g[1]]] + lis_z
+            lis_z = [[g[0] % (h // 50), g[1] % (w // 50)]] + lis_z
+        screen.fill((0, 0, 0))
         drawline(screen, size)
         drawzmei(screen, size, list)
-        pygame.display.update()
+        tab(screen, size, records, score)
+        pygame.display.flip()
+
     pygame.quit()
     
